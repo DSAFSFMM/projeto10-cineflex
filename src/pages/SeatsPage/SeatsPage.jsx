08b70eb3e884c72d731f3d90b51755ec34d921f4
Fernando_Sacerdote <fernando.saerdote@gmail.com>
2023-05-27 02:41:07 -0300

    Escolha de assento

--- src/pages/SeatsPage/SeatsPage.jsx
+++ src/pages/SeatsPage/SeatsPage.jsx
@@ -1,42 +1,88 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom"
 import styled from "styled-components"
 
 export default function SeatsPage() {
+
+    const {idSessao} = useParams();
+    const [assentos, setAssentos] = useState(null);
+    const [selecionados, setSelecionados] = useState([]);
+    const [name,setName] = useState("");
+    const [cpf, setCpf] = useState("");
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        const promisse = axios.get(`https://mock-api.driven.com.br/api/v8/cineflex/showtimes/${idSessao}/seats`);
+        promisse.then((resposta)=>{setAssentos(resposta.data.seats)});
+        promisse.catch((erro)=>console.log(erro));
+    }, []);
+
+    if (assentos === null) {
+        return(
+            <PageContainer>
+                <p>
+                    Carregando Assentos ....
+                </p>
+            </PageContainer>
+        );
+    }
+
+    function selecionaAssento(id,estado){
+        if(!estado){
+            if (selecionados.includes(id)) {
+                setSelecionados(selecionados.filter((item)=>item !== id));
+                console.log(selecionados.filter((item)=>item !== id));
+            }else{
+                setSelecionados([...selecionados, id]);
+                console.log([...selecionados, id]);
+            }
+        }else{
+            alert("Esse assento não está disponível");
+        }
+    }
+
+    function reservaAssentos(event) {
+        event.preventDefault();
+        const objeto = {ids: selecionados, name: name, cpf: cpf};
+        const post = axios.post("https://mock-api.driven.com.br/api/v8/cineflex/seats/book-many", objeto);
+        post.then(()=>navigate("/sucesso"));
+        post.catch((erro)=>console.log(erro));
+    }
+
 
     return (
         <PageContainer>
             Selecione o(s) assento(s)
 
             <SeatsContainer>
-                <SeatItem>01</SeatItem>
-                <SeatItem>02</SeatItem>
-                <SeatItem>03</SeatItem>
-                <SeatItem>04</SeatItem>
-                <SeatItem>05</SeatItem>
+                {assentos.map((el)=><SeatItem key={el.id} selecionado={selecionados.includes(el.id)} estado={el.isAvailable} onClick={()=>selecionaAssento(el.id, el.isAvailable)}>{el.name}</SeatItem>)}
             </SeatsContainer>
 
             <CaptionContainer>
                 <CaptionItem>
-                    <CaptionCircle />
+                    <CaptionCircle tipo={"selecionado"}/>
                     Selecionado
                 </CaptionItem>
                 <CaptionItem>
-                    <CaptionCircle />
+                    <CaptionCircle tipo={"disponivel"}/>
                     Disponível
                 </CaptionItem>
                 <CaptionItem>
-                    <CaptionCircle />
+                    <CaptionCircle tipo={"indisponivel"}/>
                     Indisponível
                 </CaptionItem>
             </CaptionContainer>
 
-            <FormContainer>
-                Nome do Comprador:
-                <input placeholder="Digite seu nome..." />
+            <FormContainer onSubmit={reservaAssentos}>
+                <label htmlFor="nome">Nome do Comprador:</label>
+                <input id="nome" placeholder="Digite seu nome..." value={name} onChange={(el)=>setName(el.target.value)} required/>
 
-                CPF do Comprador:
-                <input placeholder="Digite seu CPF..." />
+                <label htmlFor="cpf">CPF do Comprador:</label>
+                <input id="cpf" placeholder="Digite seu CPF..." value={cpf} onChange={(el)=>setCpf(el.target.value)} required/>
 
-                <button>Reservar Assento(s)</button>
+                <button type="submit">Reservar Assento(s)</button>
             </FormContainer>
 
             <FooterContainer>
@@ -74,7 +120,7 @@ const SeatsContainer = styled.div`
     justify-content: center;
     margin-top: 20px;
 `
-const FormContainer = styled.div`
+const FormContainer = styled.form`
     width: calc(100vw - 40px); 
     display: flex;
     flex-direction: column;
@@ -96,8 +142,32 @@ const CaptionContainer = styled.div`
     margin: 20px;
 `
 const CaptionCircle = styled.div`
-    border: 1px solid blue;         // Essa cor deve mudar
-    background-color: lightblue;    // Essa cor deve mudar
+    border: 1px solid ${(props)=>{
+        switch (props.tipo) {
+            case "selelecionado":
+                return "#0E7D71";
+            case "disponivel":
+                return "#7B8B99";
+                break;
+            case "indisponivel":
+                return "#F7C52B";
+                break;
+            default:
+                break;
+        }
+    }};         // Essa cor deve mudar
+    background-color: ${(props)=>{
+        switch (props.tipo) {
+            case "selecionado":
+                return "#1AAE9E";
+            case "disponivel":
+                return "#C3CFD9";
+            case "indisponivel":
+                return "#FBE192";
+            default:
+                break;
+        }
+    }};    // Essa cor deve mudar
     height: 25px;
     width: 25px;
     border-radius: 25px;
@@ -113,8 +183,10 @@ const CaptionItem = styled.div`
     font-size: 12px;
 `
 const SeatItem = styled.div`
-    border: 1px solid blue;         // Essa cor deve mudar
-    background-color: lightblue;    // Essa cor deve mudar
+    border: 1px solid ${(props)=> props.estado?"#F7C52B":"#7B8B99"} ;         // Essa cor deve mudar
+    background-color: ${(props)=> props.estado?"#FBE192":"#C3CFD9"};    // Essa cor deve mudar
+    border: 1px solid ${(props)=> props.selecionado?"#0E7D71":""} ;
+    background-color: ${(props)=> props.selecionado?"#1AAE9E":""}; 
     height: 25px;
     width: 25px;
     border-radius: 25px;
